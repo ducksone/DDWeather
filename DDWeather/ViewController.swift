9e731f25b64bbc23c280d6206e6435ca8734e759
duck <942342850@qq.com>
2015-12-14 15:53:30 +0800

    Initial Commit

---
+++ DDWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DDWeather
+//
+//  Created by duck on 15/12/14.
+//  Copyright © 2015年 com.duck.Weather. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
